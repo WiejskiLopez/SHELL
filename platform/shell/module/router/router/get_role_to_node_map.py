a1d04aee6b4b3365665d4091cbd5040d2ea0b3f2
WiejskiLopez <palysiewicz@poczta.onet.pl>
2026-06-01 23:07:03 +0200

    chore(F4b): remove dead FS-stage code from router and tasker
    Deleted (no longer referenced after F3 router rewrite + F4a tasker rewrite):
    
    - shell/module/router/router_stage/ (entire subtree)
    
    - shell/module/router/router/internal/: _assert_active_file_parsed, _assert_router_base_set, _assert_step_within_ttl, _distribute_active, _expire_pending_ttl, _flush_done, _parse_frontmatter, _pick_active_file, _pick_agent_output, _pick_parent_input, _rename_parent_input_as_task, _route_incoming, _seed_tasker_input_to_first_agent
    
    - shell/module/router/router/: build_frontmatter, collect_source_files, frontmatter, get_role_to_node_map, get_target_role_from_filename, load_router_params, read_metadata_from_file
    
    - shell/module/tasker/internal/: _find_node_with_input, _has_router_work, _move_router_output_to_own, _seed_task_to_first_node
    
    Kept: parse_message_filename.py (used by tasker._publish_response_envelopes to derive target_role from worker output filenames)

--- platform/shell/module/router/router/get_role_to_node_map.py
+++
@@ -1,3 +0,0 @@
-def get_role_to_node_map(graph: list) -> dict[str, dict]:
-    """Return mapping of role -> node for all nodes that have a role defined."""
-    return {n['role']: n for n in graph if n.get('role')}
